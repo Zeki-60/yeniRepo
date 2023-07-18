7af551bdfbcfa0697e80d68c664b7345567974b6
Zeki Uğuz <zekiuguz41gs156@gmail.com>
2023-07-18 19:45:30 +0300

    v6 6

--- src/main/java/armut/Armut.java
+++ src/main/java/armut/Armut.java
@@ -45,7 +45,8 @@ public class Armut {
         System.out.println("versiyon 2 kodalrım");
         System.out.println("V3 kodları");
         // v4 çalışmalar
-        // v5 çalışmalar 
+        // v5 çalışmalar
+        // v6 uhfusjff
 
     }
 }
